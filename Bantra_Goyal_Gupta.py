cda235cda50e74d784538a394a2aa650c9961f69
01dkg <deepak91g@gmail.com>
2017-04-18 12:23:50 +0100

    ok

--- Bantra_Goyal_Gupta.py
+++ Bantra_Goyal_Gupta.py
@@ -3,37 +3,22 @@ import random
 import matplotlib.pyplot as plt
 import operator
 
-n = 10 #No. of Nodes in Graph
+n = 20 #No. of Nodes in Graph
 
 def agents():
-    # G = nx.Graph()    #
-    # G.add_edge(0, 1, dist=0.8)
-    # G.add_edge(1, 2, dist=0.9)
-    # G.add_edge(0, 4, dist=1.1)
-    # G.add_edge(1, 4, dist=0.7)
-    # G.add_edge(1, 3, dist=0.1)
-    # G.add_edge(3, 6, dist=0.2)
-    # G.add_edge(3, 5, dist=0.5)
-    # G.add_edge(4, 5, dist=0.3)
-    H =  nx.erdos_renyi_graph(n, 0.3)
-    return H
-
-
+    A =  nx.erdos_renyi_graph(n, 0.4)
+    return A
 
 def customer():
-    G = nx.Graph()
-    H = agents()
-    print(nx.nodes(H))
-    G.add_edge(11,0)
-    return G
+    C = nx.Graph()
+    C.add_edge(n+1,0)
+    return C
 
-def combine():
-    A= agents()
-    C=customer()
-    H = nx.compose(A,C)
+def combine(G,C):
+    H = nx.compose(G,C)
     pos = nx.spring_layout(H)
     nx.draw_networkx_nodes(H, pos,
-                           nodelist=[11],
+                           nodelist=[n+1],
                            node_color='r',
                            node_size=500,
                            alpha=0.8)
@@ -56,50 +41,54 @@ def combine():
     labels[n+1] = r'$C$'
     nx.draw_networkx_labels(H, pos, labels, font_size=12)
     plt.show()
-combine()
+
+def  agent_status(G):
+    #Possible states of any agents
+    # 0 : Not available for work or Busy in delivering a product
+    # 1 : Available    G= agents()
+    for u in G.nodes():
+        G.node[u]["state"] = 0
+    init = random.sample(G.nodes(), 5)
+    for u in init:
+        G.node[u]["state"] = 1
+
+def agent_status_update(node,G):
+    #Setting an agent from available state to busy state
+    G[node]["state"]=0
 
 
-def agents_status():
-    G = agents()
-    Avail = {}
-    for i in G.nodes():
-        Avail[i] = random.randint(0, 1)
-    return Avail
-
-agent_available_status = agents_status()
-
-
-def agent_status_update(node):
-    agent_available_status[node]=0
-
-
-def select_available_agent():
+def select_available_agent(G):
     agent_available = []
-    for i in agents().nodes():
-        if agent_available_status[i]==1:
+    for i in G.nodes():
+        if G.node[i]["state"]==1:
             agent_available.append(i)
     del agent_available[0]
     print("Agent available", agent_available)
     return agent_available
 
 
-def best_agent():
-    agent_available = select_available_agent()
+def best_agent(G):
+    agent_available = select_available_agent(G)
     path_node ={}
     for i,values in enumerate(agent_available):
         dist = nx.dijkstra_path_length(agents(),i,0)
         path_node[values] = dist
-    node= max(path_node, key=path_node.get)
+    node= min(path_node, key=path_node.get)
     return node,agent_available
 
-
-def product_delivery():
-    node,agent_is = best_agent()
+#Mention about customer status as well
+def main():
+    G=agents()
+    C= customer()
+    agent_status(G)
+    print(G.nodes())
+    combine(G,C)
+    node, agent_is = best_agent(G)
     print(node, "agent will deliver the product")
-    print("Current Status of Agent",node," is",agent_available_status[node])
-    agent_status_update(node)
-    print("Current Status of Agent",node," is",agent_available_status[node])
+    print("Current Status of Agent", node, " is")
+    agent_status_update(node, G)
+    print("Current Status of Agent", node, " is")
     print("Product has been delivered successfully")
 
-#Mention about customer status as well
-product_delivery()
+if __name__ == "__main__":
+    main()
